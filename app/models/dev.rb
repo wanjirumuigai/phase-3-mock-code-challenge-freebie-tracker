b32912f1df4021e98dd258df8e88277e69f82fbe
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-31 21:51:25 +0300

    received_one and give_away instance methods

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,17 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.map { |freebie| freebie.item_name }.include?(item_name)
+  end
+
+  def give_away(dev, freebie)
+    if self.freebies.include?(freebie)
+      freebie.dev_id = dev.id
+      freebie.save
+    else
+      "Not yours to give"
+    end
+  end
 end
